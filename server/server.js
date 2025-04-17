8ca9aa459365a6c0962ba4e4b6898fad4c0d110b
shivammishra6 <shivammmishra6203@gmail.com>
2025-04-17 22:11:06 +0530

    changed few things

--- server/server.js
+++ server/server.js
@@ -14,10 +14,10 @@ app.use(express.json());
 app.use('/api/jobs',router)
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+    app.use(express.static(path.join(__dirname,"/client/dist")))
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+        res.sendFile(path.resolve(__dirname,"client","dist","index.html"))
     })
     
 }
